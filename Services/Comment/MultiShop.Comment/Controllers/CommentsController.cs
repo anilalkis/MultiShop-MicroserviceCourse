22c30ab96f53b95b4d90929e414ada98b9184543
anilalkis <anilalkis89@gmail.com>
2024-08-26 17:27:16 +0300

    Comment service done.

---
+++ Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Comment.Context;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentContext _commentContext;
+
+        public CommentsController(CommentContext commentContext)
+        {
+            _commentContext = commentContext;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList()
+        {
+            var values = _commentContext.UserComments.ToList();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(UserComment userComment)
+        {
+            var values = _commentContext.UserComments.Add(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Yorum başarıyla eklendi.");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateComment(UserComment userComment)
+        {
+            var values = _commentContext.UserComments.Update(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Yorum başarıyla güncellendi.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComment(int id)
+        {
+            var values = _commentContext.UserComments.Find(id);
+            _commentContext.Remove(values);
+            _commentContext.SaveChanges();
+            return Ok("Yorum başarıyla silindi.");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetComment(int id)
+        {
+            var values = _commentContext.UserComments.Find(id);
+            return Ok(values);
+        }
+    }
+}

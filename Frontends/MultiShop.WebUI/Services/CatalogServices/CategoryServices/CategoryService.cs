deea2a3d11ce8315091043156cdc0241e939ac47
anilalkis <anilalkis89@gmail.com>
2024-09-27 13:25:29 +0300

    client credential token

---
+++ Frontends/MultiShop.WebUI/Services/CatalogServices/CategoryServices/CategoryService.cs
@@ -0,0 +1,50 @@
+﻿using MultiShop.DtoLayer.CatalogDtos.BrandDtos;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using MultiShop.WebUI.ViewComponents.UILayoutViewComponents;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MultiShop.WebUI.Services.CatalogServices.CategoryServices
+{
+	public class CategoryService : ICategoryService
+	{
+		private readonly HttpClient _httpClient;
+
+		public CategoryService(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		public async Task CreateCategoryAsync(CreateCategoryDto createCategoryDto)
+		{
+			await _httpClient.PostAsJsonAsync<CreateCategoryDto>("categories", createCategoryDto);
+		}
+
+		public async Task DeleteCategoryAsync(string id)
+		{
+			await _httpClient.DeleteAsync("categories?id=" + id);
+		}
+
+		public async Task<List<ResultCategoryDto>> GetAllCategoriesAsync()
+		{
+			var responseMessage = await _httpClient.GetAsync("categories");
+			var jsonData = await responseMessage.Content.ReadAsStringAsync();
+			var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+			return values;
+		}
+
+		public async Task<GetByIdCategoryDto> GetByIdCategoryAsync(string id)
+		{
+			var responseMessage = await _httpClient.GetAsync("categories/" + id);
+			var jsonData = await responseMessage.Content.ReadAsStringAsync();
+			var values = JsonConvert.DeserializeObject<GetByIdCategoryDto>(jsonData);
+
+			return values;
+		}
+
+		public async Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto)
+		{
+			await _httpClient.PutAsJsonAsync("categories", updateCategoryDto);
+		}
+	}
+}
